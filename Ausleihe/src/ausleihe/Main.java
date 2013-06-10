0fb8638647fd8dce8e6915a310e0a4675e6c0e0f
Cookieforge <pr.geld@web.de>
2013-06-10 19:34:32 +0200

    Erster Fehlerhafter Versuch auf die Datenbank zuzugreifen.

--- Ausleihe/src/ausleihe/Main.java
+++ Ausleihe/src/ausleihe/Main.java
@@ -1,13 +1,37 @@
 package ausleihe;
 
+import java.sql.*;
+
 public class Main {
 
     /**
      * @param args
      */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
-    }
+	public static void main(String[] args) throws SQLException {
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try{
+			//Ich hasse euch alle, gez. Keks
+			Class.forName("net.sourceforge.jtds.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:jtds:sqlserver://127.0.0.1/ProjectAusleihe","","");
+			stmt = con.createStatement( ) ;
+			rs = stmt.executeQuery("Select * From adressen;");
+			while(rs.next( )){
+				System.out.println(rs.getString("Nachname") + rs.getString("Vorname"));
+			}
+		} catch ( Exception e ) {
+			System.out.println("fehler"); 
+			e.printStackTrace();
+		} finally {
+			if (rs != null) rs.close ( ) ;
+			if (stmt != null) stmt.close ( ) ;
+			if (con != null) con.close ( );
+			System.out.println("Fertig");
+			
+		}
+		
+		
+	}	
 
 }
