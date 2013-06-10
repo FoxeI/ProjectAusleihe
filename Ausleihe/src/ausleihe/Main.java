f622c1eab84a30d869409aa9c2f4a040b6b9c6e6
FoxeI <storkner@googlemail.com>
2013-06-10 18:22:46 +0200

    Initial Commit -- Added Class Main

---
+++ Ausleihe/src/ausleihe/Main.java
@@ -0,0 +1,13 @@
+package ausleihe;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
